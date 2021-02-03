93e12c15c6c73e434d7208a699a33aedc755bb38
Filip <www.filipx@gmail.com>
2021-02-03 19:45:37 +0100

    Separate username and password boxes on login page

--- netos/urls.py
+++ netos/urls.py
@@ -2,6 +2,7 @@
 # netos/urls.py
 
 from django.conf.urls import url
+from django.contrib import admin
 from django.urls import path
 
 from . import views             # import of application views
@@ -11,5 +12,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.loginPage, name='login'),
     url(r'^logout/$', views.logoutPage, name='logout'),
-    path('<str:room_name>/', views.room, name='room'),
+    # url(r'^<str:room_name>/$', views.room, name='room'),
 ]
