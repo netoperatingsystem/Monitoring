3cef7325dbb3db95b7358dab83c4a1eb0d56d24b
Filip1512 <filipx1512@gmail.com>
2021-08-21 08:13:55 -0700

    Capture first data from devices

--- netos/models.py
+++ netos/models.py
@@ -11,6 +11,7 @@ class Device(models.Model):
     def __str__(self):
         return self.name
 
+
 class Labipaddress(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
--- netos/snmppoller.py
+++ netos/snmppoller.py
@@ -1,23 +1,23 @@
 import csv
 import schedule
 import time
-from easysnmp import Session
+from easysnmp import *
 
 
 def poll(host, com, ver, mib):
     session = Session(hostname=host, community=com, version=ver)
     output = session.get(mib)
-    print(output)
+    print(output.value)
 
-    with open('inventory.csv') as inventory:
-        invcsv = csv.reader(inventory)
-        for row in invcsv:
-            host = row[0]
-            freq = int(row[1])
-            com = row[2]
-            ver = int(row[3])
-            for mib in row[4:]:
-                schedule.every(freq).seconds.do(poll, host, com, ver, mib)
+with open('inventory.csv') as inventory:
+    invcsv = csv.reader(inventory)
+    for row in invcsv:
+        host = row[0]
+        freq = int(row[1])
+        com = row[2]
+        ver = int(row[3])
+        for mib in row[4:]:
+            schedule.every(freq).seconds.do(poll, host, com, ver, mib)
 
 
 while True:
